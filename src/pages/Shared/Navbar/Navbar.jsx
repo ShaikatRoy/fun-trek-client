4c93620a24e6b649f259d9c32909a4205d4934bf
Shaikat roy <shaikatroy258@gmail.com>
2023-06-09 10:47:38 +0600

    login page done

--- src/pages/Shared/Navbar/Navbar.jsx
+++ src/pages/Shared/Navbar/Navbar.jsx
@@ -1,24 +1,40 @@
 import { Link } from "react-router-dom";
 import { FaFacebook, FaInbox, FaInstagram, FaLinkedinIn, FaPhone, FaTwitter, FaYoutube } from 'react-icons/fa';
+import { useContext } from "react";
+import { AuthContext } from "../../../providers/AuthProvider";
 const Navbar = () => {
-
-    const navOptions = <>
+    const {user, logOut} = useContext(AuthContext);
+    
+    const handleLogout = () => {
+        logOut()
+        .then(() => {})
+        .catch(error => console.log(error));
+    }
+    
+    const navOptions = <> 
         <li><Link to="/">Home</Link></li>
         <li><Link to="/instructors">Instructors</Link></li>
         <li><Link to="/classes">Classes</Link></li>
         <li><Link to="/dashboard">Dashboard</Link></li>
-        <li><Link to="/login">login</Link></li>
-    
+        
+        {
+            user? <> 
+                <button onClick={handleLogout} className="btn btn-ghost">LogOut</button>
+            </> : <>
+                <li><Link to="/login">login</Link></li>
+            </>
+        }
+
     </>
 
     return (
         <>
-        <div className="footer items-center p-4 bg-transparent bg-black max-w-screen-xl text-black">
+            <div className="footer items-center p-4 bg-transparent bg-black max-w-screen-xl text-black">
                 <div className="items-center grid-flow-col">
-                   <div className="flex">
-                   <p className="flex me-5"><FaInbox className="me-2"></FaInbox> Info@printers.com</p>
-                    <p className="flex"><FaPhone className="me-2"></FaPhone> +91 0800 123 4567</p>
-                   </div>
+                    <div className="flex">
+                        <p className="flex me-5"><FaInbox className="me-2"></FaInbox> Info@printers.com</p>
+                        <p className="flex"><FaPhone className="me-2"></FaPhone> +91 0800 123 4567</p>
+                    </div>
                 </div>
                 <div className="grid-flow-col gap-4 place-self-center md:justify-self-end">
                     <Link><FaLinkedinIn></FaLinkedinIn></Link>
@@ -28,7 +44,7 @@ const Navbar = () => {
                     <Link><FaYoutube></FaYoutube></Link>
                 </div>
             </div>
-            
+
             <div className="navbar fixed z-10 bg-opacity-30 bg-black max-w-screen-xl text-white">
                 <div className="navbar-start">
                     <div className="dropdown">
@@ -46,8 +62,10 @@ const Navbar = () => {
                         {navOptions}
                     </ul>
                 </div>
-                <div className="navbar-end">
-                    <a className="btn">Get started</a>
+                <div className="avatar navbar-end">
+                    <div className="w-14 mask mask-squircle">
+                        <img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+                    </div>
                 </div>
             </div>
         </>
--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,85 +1,127 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { Helmet } from "react-helmet-async";
 import { useForm } from "react-hook-form";
 import { AuthContext } from "../../providers/AuthProvider";
 
-
 const SignUp = () => {
-    const { register, handleSubmit, formState: { errors } } = useForm();
-    const {createUser} = useContext(AuthContext);
-  
-    const onSubmit = data => {
-        console.log(data);
-        createUser(data.email, data.password, data.photoURL, data.name)
-        .then(result => {
-            const loggedUser = result.user;
-            console.log(loggedUser);
-        })
-    };
+  const { register, handleSubmit, formState: { errors }, getValues } = useForm();
+  const { createUser } = useContext(AuthContext);
+  const [passwordError, setPasswordError] = useState("");
 
-    return (
-        <>
-        <Helmet>
-            <title>Fun Trek | Sign Up</title>
-        </Helmet>
-        <section  className="bg-white dark:bg-gray-900 rounded-2xl">
-            <div className="container flex items-center justify-center min-h-screen px-6 mx-auto">
-                <form onSubmit={handleSubmit(onSubmit)} className="w-full max-w-md">
-                    <div className="flex items-center justify-center mt-6">
-                        <a href="#" className="w-1/3 pb-4 font-medium text-center text-gray-800 capitalize border-b-2 border-blue-500 dark:border-blue-400 dark:text-white">
-                            sign up
-                        </a>
-                    </div>
+  const onSubmit = data => {
+    const { password, confirmPassword } = data;
+    
+    if (password !== confirmPassword) {
+      setPasswordError("Passwords do not match");
+      return;
+    }
+    
+    setPasswordError("");
 
-                    <div className=" form-control mt-8">
-                        <input type="text" {...register("name", { required: true })} className="block w-full py-3 text-gray-700 bg-white border rounded-lg px-11 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40" placeholder="Username" />
-                        {errors.name && <span className="text-red-500">Name is required</span>}
-                    </div>
+    console.log(data);
+    createUser(data.email, data.password, data.photoURL, data.name)
+      .then(result => {
+        const loggedUser = result.user;
+        console.log(loggedUser);
+      });
+  };
 
-                    <div className=" form-control mt-8">
-                        <input type="url" {...register("photoURL", { required: true })} className="block w-full py-3 text-gray-700 bg-white border rounded-lg px-11 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40" placeholder="Photo URL" />
-                        {errors.url && <span className="text-red-500">Name is required</span>}
-                    </div>
-
-                    <div className="form-control mt-6">
-                        <input type="email" {...register("email", { required: true })} className="block w-full py-3 text-gray-700 bg-white border rounded-lg px-11 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40" placeholder="Email address" />
-                        {errors.email && <span className="text-red-500">Email is required</span>}
-                    </div>
-
-                    <div className="form-control mt-4">
-                       
-                        <input type="password" {...register("password", { 
-                            required: true, 
-                            minLength: 6, 
-                            maxLength: 20,
-                            pattern: /(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])(?=.*[a-z])/
-                              })} className="block w-full px-10 py-3 text-gray-700 bg-white border rounded-lg dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40" placeholder="Password" />
-                                 {errors.password?.type === 'required' && <span className="text-red-500">password is required</span>}
-                                {errors.password?.type === 'minLength' && <span className="text-red-500">password must be 6 characters</span>}
-                                {errors.password?.type === 'maxLength' && <span className="text-red-500">password must be less then 20 characters</span>}
-                                {errors.password?.type === 'pattern' && <span className="text-red-500">password must have one lower case, one uppercase, one number and one special characters</span>}
-                    </div>
-
-                    <div className="form-control mt-4">
-                         <input type="password" className="block w-full px-10 py-3 text-gray-700 bg-white border rounded-lg dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40" placeholder="Confirm Password" />
-                    </div>
-
-                    <div className="mt-6">
-                        <button type="submit" className="w-full px-6 py-3 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-500 rounded-lg hover:bg-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-50">
-                            Sign Up
-                        </button>
-
-                        <div className="mt-6 text-center">
-                            <a href="/login" className="text-sm text-blue-500 hover:underline dark:text-blue-400">
-                                Already have an account?
-                            </a>
-                        </div>
-                    </div>
-                </form>
+  return (
+    <>
+      <Helmet>
+        <title>Fun Trek | Sign Up</title>
+      </Helmet>
+      <section className="bg-white dark:bg-gray-900 rounded-2xl">
+        <div className="container flex items-center justify-center min-h-screen px-6 mx-auto">
+          <form onSubmit={handleSubmit(onSubmit)} className="w-full max-w-md">
+            <div className="flex items-center justify-center mt-6">
+              <a href="#" className="w-1/3 pb-4 font-medium text-center text-gray-800 capitalize border-b-2 border-blue-500 dark:border-blue-400 dark:text-white">
+                sign up
+              </a>
             </div>
-        </section>
+
+            <div className="form-control mt-8">
+              <input
+                type="text"
+                {...register("name", { required: true })}
+                className="block w-full py-3 text-gray-700 bg-white border rounded-lg px-11 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
+                placeholder="Username"
+              />
+              {errors.name && <span className="text-red-500">Name is required</span>}
+            </div>
+
+            <div className="form-control mt-8">
+              <input
+                type="url"
+                {...register("photoURL", { required: true })}
+                className="block w-full py-3 text-gray-700 bg-white border rounded-lg px-11 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
+                placeholder="Photo URL"
+              />
+              {errors.photoURL && <span className="text-red-500">Photo URL is required</span>}
+            </div>
+
+            <div className="form-control mt-6">
+              <input
+                type="email"
+                {...register("email", { required: true })}
+                className="block w-full py-3 text-gray-700 bg-white border rounded-lg px-11 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
+                placeholder="Email address"
+              />
+              {errors.email && <span className="text-red-500">Email is required</span>}
+            </div>
+
+            <div className="form-control mt-4">
+              <input
+                type="password"
+                {...register("password", {
+                  required: true,
+                  minLength: 6,
+                  maxLength: 20,
+                  pattern: /(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])(?=.*[a-z])/
+                })}
+                className="block w-full px-10 py-3 text-gray-700 bg-white border rounded-lg dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
+                placeholder="Password"
+              />
+              {errors.password?.type === 'required' && <span className="text-red-500">Password is required</span>}
+              {errors.password?.type === 'minLength' && <span className="text-red-500">Password must be at least 6 characters</span>}
+              {errors.password?.type === 'maxLength' && <span className="text-red-500">Password must be less than 20 characters</span>}
+              {errors.password?.type === 'pattern' && <span className="text-red-500">Password must have one lowercase, one uppercase, one number, and one special character</span>}
+            </div>
+
+            <div className="form-control mt-4">
+              <input
+                type="password"
+                {...register("confirmPassword", {
+                  required: true,
+                  validate: value => value === getValues().password || "Passwords do not match"
+                })}
+                className="block w-full px-10 py-3 text-gray-700 bg-white border rounded-lg dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
+                placeholder="Confirm Password"
+              />
+              {errors.confirmPassword?.type === 'required' && <span className="text-red-500">confirm password</span>}
+              {errors.confirmPassword && <span className="text-red-500">{errors.confirmPassword.message}</span>}
+              {passwordError && <span className="text-red-500">{passwordError}</span>}
+            </div>
+
+            <div className="mt-6">
+              <button
+                type="submit"
+                className="w-full px-6 py-3 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-500 rounded-lg hover:bg-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-50"
+              >
+                Sign Up
+              </button>
+
+              <div className="mt-6 text-center">
+                <a href="/login" className="text-sm text-blue-500 hover:underline dark:text-blue-400">
+                  Already have an account?
+                </a>
+              </div>
+            </div>
+          </form>
+        </div>
+      </section>
     </>
-    );
+  );
 };
 
 export default SignUp;
