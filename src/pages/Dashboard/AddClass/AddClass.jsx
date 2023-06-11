ed7a5f6ed92e30e2707c6dcc1db5b2357c8794cc
Shaikat roy <shaikatroy258@gmail.com>
2023-06-12 02:06:22 +0600

    classes added to the database

--- src/pages/Dashboard/AddClass/AddClass.jsx
+++ src/pages/Dashboard/AddClass/AddClass.jsx
@@ -1,10 +1,135 @@
 
+import { useForm } from 'react-hook-form';
+import useAuth from '../../../hooks/useAuth';
+
 const AddClass = () => {
-    return (
-        <div>
-            <h2>Add Class here</h2>
+  const { user } = useAuth();
+  const { register, handleSubmit, reset } = useForm();
+
+  const onSubmit = (data) => {
+    const saveClass = {
+      className: data.className,
+      classImage: data.classImage,
+      name: data.name,
+      profileImage: data.profileImage,
+      email: data.email,
+      seat: data.seat,
+      price: data.price,
+      status: 'pending'
+    };
+  
+    const token = localStorage.getItem('access-token');
+    console.log('Token:', token);
+  
+    fetch('http://localhost:5000/classes', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify(saveClass),
+    })
+      .then((response) => response.json())
+      .then((result) => {
+        console.log(result);
+        reset();
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+  
+
+  return (
+    <div className="w-full">
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="form-control w-full mx-5">
+          <label className="label mx-5">
+            <span className="label-text">Class Name</span>
+          </label>
+          <input
+            type="text"
+            placeholder="class name"
+            className="input input-bordered w-full mx-5"
+            {...register('className', { required: true })}
+          />
         </div>
-    );
+        <div className="form-control w-full mx-5">
+          <label className="label mx-5">
+            <span className="label-text">Class Image</span>
+          </label>
+          <input
+            type="text"
+            placeholder="input image url"
+            className="input input-bordered w-full mx-5"
+            {...register('classImage', { required: true })}
+          />
+        </div>
+        <div className="form-control w-full mx-5">
+          <label className="label mx-5">
+            <span className="label-text">Name</span>
+          </label>
+          <input
+            type="text"
+            defaultValue={user?.displayName}
+            readOnly
+            placeholder="name"
+            className="input input-bordered w-full mx-5"
+            {...register('name', { required: true })}
+          />
+        </div>
+        <div className="form-control w-full mx-5">
+          <label className="label mx-5">
+            <span className="label-text">Profile Image</span>
+          </label>
+          <input
+            type="text"
+            placeholder="input image url"
+            className="input input-bordered w-full mx-5"
+            {...register('profileImage', { required: true })}
+          />
+        </div>
+        <div className="flex">
+          <div className="form-control w-full mx-5">
+            <label className="label mx-5">
+              <span className="label-text">Email</span>
+            </label>
+            <input
+              type="text"
+              defaultValue={user?.email}
+              readOnly
+              placeholder="email"
+              className="input input-bordered w-full mx-5"
+              {...register('email', { required: true })}
+            />
+          </div>
+          <div className="form-control w-full ms-5">
+            <label className="label ">
+              <span className="label-text">Seat</span>
+            </label>
+            <input
+              type="text"
+              placeholder="seat"
+              className="input input-bordered w-full "
+              {...register('seat', { required: true })}
+            />
+          </div>
+        </div>
+        <div className="form-control w-full mx-5">
+          <label className="label mx-5">
+            <span className="label-text">Price</span>
+          </label>
+          <input
+            type="text"
+            placeholder="price"
+            className="input input-bordered w-full mx-5"
+            {...register('price', { required: true })}
+          />
+        </div>
+        <input type="submit" className="flex btn btn-outline mx-auto my-3" value="Submit" />
+      </form>
+    </div>
+  );
 };
 
 export default AddClass;
